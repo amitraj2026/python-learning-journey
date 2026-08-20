3b5320439183af8b69f53cd4dbe49398e377286a
Amit Raj <aramitraj7292@gmail.com>
2026-08-20 08:24:51 +0530

    Update 02_add.py

--- 05_user_input/02_add.py
+++ 05_user_input/02_add.py
@@ -1,1 +1,6 @@
 
+a = int(input("Enter first number.."))
+# a = int(a) # Convert a to int version of a 
+b = int(input("Enter second number"))
+# b = int(b) # Convert b (str) to int version of b
+print(a + b)
