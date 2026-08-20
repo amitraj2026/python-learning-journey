96ec984c739d1f47a310ba32027a7f9a9684e4cd
Amit Raj <aramitraj7292@gmail.com>
2026-08-20 23:33:39 +0530

    Create 01_text_analyzer.py

---
+++ 10_string/01_text_analyzer.py
@@ -0,0 +1,11 @@
+text = input("Enter your text here...  ")
+
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.strip())
+print(text.split())
+print(text.count("k"))
+print(len(text))
+
+
