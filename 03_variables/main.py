d8d05534f93e7c04efe8d0c2a267c5c9f268d16e
Amit Raj <aramitraj7292@gmail.com>
2026-08-20 08:20:36 +0530

    Update main.py

--- 03_variables/main.py
+++ 03_variables/main.py
@@ -1,1 +1,27 @@
 
+age = 34 # integer
+name = "Sachin" #string
+cgps = 8.3 #float
+
+# Rule of defining a variable in python
+# Variable name must start with (a-z, A-Z) or an underscore (_).
+# They can store letters, numbers, and underscore.
+# Variable name are case sensitive (age and Age are different).
+# Avoid using python keywords (e.g., if, for, while) as a variable names. 
+
+
+# 34age = 4 #Invalid because variable cannot start with a number .
+print(age)
+print(type(age))
+
+print(name)
+print(type(name))
+
+cgpa = 8.3
+print(cgpa)
+print(type(cgpa))
+
+
+is_completed = True
+print(is_completed)
+print(type(is_completed))
