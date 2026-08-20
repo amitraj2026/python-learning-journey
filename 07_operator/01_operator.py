df0f47e559100142c4b9a5a89929187083a9ba94
Amit Raj <aramitraj7292@gmail.com>
2026-08-20 08:44:51 +0530

    Update 01_operator.py

--- 07_operator/01_operator.py
+++ 07_operator/01_operator.py
@@ -1,1 +1,21 @@
 
+a = 34
+b = 2
+
+
+# Arithmetic Operators
+print("a + b = ", a+b)
+print("a - b = ", a-b)
+print("a / b = ", a/b)
+print("a * b = ", a*b)
+print("a // b = ", a//b)
+print("b ** b = ", b**b)
+
+
+
+
+
+# Condiational Operators
+print(a>4)
+print(a==4)
+print(a>=4)
