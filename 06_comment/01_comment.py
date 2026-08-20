6d2c546822b80960bf652e9fbc1358e39af1bd4e
Amit Raj <aramitraj7292@gmail.com>
2026-08-20 08:26:46 +0530

    Update 01_comment.py

--- 06_comment/01_comment.py
+++ 06_comment/01_comment.py
@@ -1,1 +1,32 @@
 
+print("hii my name is Amit")
+
+
+'''
+hjh
+jjh
+hjh
+jjhhjh
+jjhhjh
+jjhhjh
+jjhhjh '''
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjhhjh
+# jjh
