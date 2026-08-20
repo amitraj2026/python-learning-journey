0c9d61ba44477fcd4b6d776dbec7d6a296d03edf
Amit Raj <aramitraj7292@gmail.com>
2026-08-20 08:48:47 +0530

    Create 03_if_elif.py

---
+++ 08_conditionals/03_if_elif.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age "))
+
+if(age>18):
+    print("you can drive.")
+elif(age==18):
+    print("Let`s schedule a interview")
+elif(age==0):
+    print("Buddy you are just born.")
+else:
+    print("You can not drive")
