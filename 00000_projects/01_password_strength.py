a2ce33ad79b9d1b3a93314a406a143e3af67e437
Amit Raj <aramitraj7292@gmail.com>
2026-08-21 23:39:09 +0530

    Update 01_password_strength.py

--- 00000_projects/01_password_strength.py
+++ 00000_projects/01_password_strength.py
@@ -1,1 +1,10 @@
+password = input("Enter your password")
+pas_lenth = len(password)
+if(pas_lenth<8):
+    print("Not valid! Your password is below 8 characters.")
 
+print(password.isalpha())
+print(password.isdigit())
+print(password.isalnum())
+print(password.isspace())
+# print(password.isalpha())
