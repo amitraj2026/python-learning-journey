b17833c3280ee4d5cdfe7d0d365cbada86cbb355
Amit Raj <aramitraj7292@gmail.com>
2026-08-21 23:42:46 +0530

    Create age_calculator.py

---
+++ 00_projects/Day1/age_calculator.py
@@ -0,0 +1,9 @@
+currrent_year = 2026
+birth_year = int(input("Enter your birth year"))
+
+
+age = 0 
+if(currrent_year>birth_year):
+    age = currrent_year - birth_year
+
+print(f"Your age is: {age}")
