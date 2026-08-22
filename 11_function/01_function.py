a07a3f5341ec4238021ca870aaa214e1ddb4d054
Amit Raj <aramitraj7292@gmail.com>
2026-08-22 20:36:00 +0530

    Create 01_function.py

---
+++ 11_function/01_function.py
@@ -0,0 +1,21 @@
+# a = 4
+# b = 2
+# c = 9
+
+
+# average = (a+b+c)/3
+# print(average)
+
+
+# Just define the function
+def average(a, b, c):  #function start with def
+    d = (a+b+c)/3
+    # print(d)
+    return d
+
+
+o1 = average(3, 5, 1)
+o2 = average(3, 8, 4)
+
+
+print(o1)
